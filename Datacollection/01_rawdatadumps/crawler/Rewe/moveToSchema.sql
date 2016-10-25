06b71c1e06c8813650968bd34508ba4a01b6626d
Michael Schreier <schreier@in.tum.de>
2016-10-25 16:05:37 +0200

    Added products to categorie

--- Datacollection/01_rawdatadumps/crawler/Rewe/moveToSchema.sql
+++ Datacollection/01_rawdatadumps/crawler/Rewe/moveToSchema.sql
@@ -7,9 +7,9 @@ BEGIN
 END
 $$;
 
-
 DROP TABLE IF EXISTS rewerawinsertshoptmp;
 DROP TABLE IF EXISTS rewerawinsertcategorytmp;
+DROP TABLE IF EXISTS rewerawinsertcategorytmp2;
 DROP TABLE IF EXISTS rewerawinsertarticletmp;
 DROP TABLE IF EXISTS rewerawinsertattributmp;
 
@@ -23,13 +23,36 @@ CREATE TABLE rewerawinsertshoptmp AS
 Select nextval('shop_sid_seq'::regclass) sid, base.* FROM (Select distinct 'REWE' as name, 'https://shop.rewe.de/' as url from rewerawdata) base;
 
 CREATE TABLE rewerawinsertcategorytmp AS 
-Select nextval('category_caid_seq'::regclass) as caid,base.* FROM (Select distinct value as name,NULL::INTEGER as pcaid,sid,NULL::TEXT  as url from rewerawdata r cross join (Select * FROM rewerawinsertshoptmp where name='REWE' LIMIT 1) s where key='Category') base;
+Select nextval('category_caid_seq'::regclass) as caid,base.*
+FROM (
+	Select distinct value as name,NULL::INTEGER as pcaid,sid,NULL::TEXT  as url
+	from rewerawdata r cross join (Select * FROM rewerawinsertshoptmp where name='REWE' LIMIT 1) s 
+	where key='Category'
+) base;
+
+CREATE TABLE rewerawinsertcategorytmp2 AS 
+Select nextval('category_caid_seq'::regclass) as caid,base.*
+FROM (
+	Select distinct rc.value as name,cp.caid as pcaid,s.sid,NULL::TEXT  as url
+	from rewerawdata rc join rewerawdata rp on rc.rowid=rp.rowid
+	join rewerawinsertcategorytmp cp on cp.name = rp.value
+	cross join (Select * FROM rewerawinsertshoptmp where name='REWE' LIMIT 1) s 
+	where rc.key='Category2' and rp.key='Category'
+) base;
 
 CREATE TABLE rewerawinsertarticletmp AS
-Select r1.rowid,nextval('article_arid_seq'::regclass) arid,r1.value as name,r1.source as url,c.caid as caid from rewerawdata r1 join rewerawdata r2 on r1.title=r2.title and r1.source=r2.source and r1.rowid=r2.rowid join rewerawinsertcategorytmp c on c.name=r2.value WHERE r1.key='Article' and r2.key='Category';
+Select r1.rowid,nextval('article_arid_seq'::regclass) arid,r1.value as name,r1.source as url,cc.caid as caid
+from rewerawdata r1 join rewerawdata rcc on r1.rowid=rcc.rowid
+join rewerawdata rcp on r1.rowid=rcp.rowid
+join rewerawinsertcategorytmp2 cc on cc.name=rcc.value
+join rewerawinsertcategorytmp cp on cp.name=rcp.value and cc.pcaid=cp.caid
+WHERE r1.key='Article' and rcc.key='Category2' and rcp.key='Category';
+
 
 CREATE TABLE rewerawinsertattributmp AS
-Select nextval('attribute_atid_seq'::regclass) as atid,r.key as name, r.value as content,at.arid from rewerawdata r join rewerawinsertarticletmp at on r.rowid=at.rowid;
+Select nextval('attribute_atid_seq'::regclass) as atid,r.key as name, r.value as content,at.arid
+from rewerawdata r
+join rewerawinsertarticletmp at on r.rowid=at.rowid;
 
 DO language plpgsql $$
 BEGIN
@@ -41,6 +64,9 @@ Select * from rewerawinsertshoptmp;
 
 INSERT INTO category (caid,name,pcaid,sid,url)
 Select * from rewerawinsertcategorytmp;
+
+INSERT INTO category (caid,name,pcaid,sid,url)
+Select * from rewerawinsertcategorytmp2;
 
 Insert INTO article (arid,name,url,caid)
 Select arid,name,url,caid FROM rewerawinsertarticletmp;
@@ -55,7 +81,7 @@ $$;
 
 DROP TABLE IF EXISTS rewerawinsertshoptmp;
 DROP TABLE IF EXISTS rewerawinsertcategorytmp;
+DROP TABLE IF EXISTS rewerawinsertcategorytmp2;
 DROP TABLE IF EXISTS rewerawinsertarticletmp;
 DROP TABLE IF EXISTS rewerawinsertattributmp;
 commit;
-
