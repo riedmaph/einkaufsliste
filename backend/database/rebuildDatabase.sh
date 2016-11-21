7168082385d78b8c2280278199c2719fc24aab6d
Philipp R <badphil@googlemail.com>
2016-11-21 21:43:44 +0100

    make rebuildDatabase ready for RDS

--- backend/database/rebuildDatabase.sh
+++ backend/database/rebuildDatabase.sh
@@ -2,7 +2,12 @@
 
 . config.sh
 
-read -s -p "Please enter postgres admin password: " pspw
+read -p "Please enter postgres admin user (default: postgres): " pgun
+read -s -p "Please enter postgres admin password: " pgpw
 
-PGPASSWORD=$pspw psql -h $dbhost -p $dbport -U postgres -a -f "dropDb.sql" -v dbname=$dbname -v dbusercrawler=$dbusercrawler -v dbusertransformer=$dbusertransformer -v dbuserapi=$dbuserapi; 
-PGPASSWORD=$pspw psql -h $dbhost -p $dbport -U postgres -a -f "createDb.sql" -v dbname=$dbname -v dbusercrawler=$dbusercrawler -v dbpasscrawler=$dbpasscrawler -v dbusertransformer=$dbusertransformer -v dbpasstransformer=$dbpasstransformer -v dbuserapi=$dbuserapi -v dbpassapi=$dbpassapi; 
+if [ -z $pgun ]; then
+	pgun="postgres"
+fi
+
+PGPASSWORD=$pgpw psql -h $dbhost -p $dbport -U $pgun -d postgres -a -f "dropDb.sql" -v dbname=$dbname -v dbusercrawler=$dbusercrawler -v dbusertransformer=$dbusertransformer -v dbuserapi=$dbuserapi; 
+PGPASSWORD=$pgpw psql -h $dbhost -p $dbport -U $pgun -d postgres -a -f "createDb.sql" -v dbname=$dbname -v dbusercrawler=$dbusercrawler -v dbpasscrawler=$dbpasscrawler -v dbusertransformer=$dbusertransformer -v dbpasstransformer=$dbpasstransformer -v dbuserapi=$dbuserapi -v dbpassapi=$dbpassapi -v dbadmin=$pgun; 
