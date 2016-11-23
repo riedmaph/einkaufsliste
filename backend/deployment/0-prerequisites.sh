2a6ea2c8165cad8845dd64a4499ef958cf270fff
Philipp R <badphil@googlemail.com>
2016-11-23 22:46:00 +0100

    added backend deploy scripts

---
+++ backend/deployment/0-prerequisites.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This file is NOT executed by autodeploy!
+
+# Install node.js and npm
+apt-get install -y nodejs npm
--- backend/deployment/4-applicationStart.sh
+++ backend/deployment/4-applicationStart.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+# Here is the place to start the application
+
+# start node.js webserver as service
+systemctl start elisa-api
