b3e1b75af299c21369e2e93f54886e432a4879a2
gernot303 <mail@gernotbrunner.at>
2016-12-03 23:00:38 -0800

    * replace spaces with tabs in dbhandler.js (got lost by merge of task-0-gb-api-unittests)

--- backend/server/test/dbhandler/dbhandler.js
+++ backend/server/test/dbhandler/dbhandler.js
@@ -4,15 +4,13 @@ var db = require(path.join('..', '..', 'controllers','dbconnector.js'));
 var sqlDeletetUser = db.loadSql(path.join('test', 'dbhandler', 'sqlDeletetUser.sql'));
 
 module.exports.deleteUsers = function (callback) {
-
-  db.conn.none(sqlDeletetUser)
-    .then(function () {
-    		callback();
-    })
-    .catch(function (err) {
-    		console.log(err.message);
-      callback(err);
-    });
-
+	db.conn.none(sqlDeletetUser)
+		.then(function () {
+			callback();
+		})
+		.catch(function (err) {
+			console.log(err.message);
+			callback(err);
+		});
 }
 
