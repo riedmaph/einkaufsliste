fd450cad2deb8eb05dbdc67834146fbe0dc51f68
gernot303 <mail@gernotbrunner.at>
2016-12-09 02:41:27 -0800

    * fix comments in moveitemDown/Up.sql

--- backend/server/controllers/items/moveItemUp.sql
+++ backend/server/controllers/items/moveItemUp.sql
@@ -1,16 +1,16 @@
-/* Beginn Transaction targetposition secure atargetpositionmic execution*/
+/* Beginn Transaction to secure atomic execution*/
 BEGIN;
 /* free position of from object */
 UPDATE ${schemaname:raw}.Item 
 SET position=-1
 WHERE list=${listid} AND position = ${from};
 
-/* move items down targetposition free targetposition position*/
+/* move items down to free targetposition position*/
 UPDATE ${schemaname:raw}.Item 
 SET position=position+1
 WHERE list=${listid} AND position >= ${targetposition} AND position < ${from};
 
-/*set from item's position targetposition targetposition position*/
+/*set from item's position to targetposition position*/
 UPDATE ${schemaname:raw}.Item 
 SET position=${targetposition}
 WHERE list=${listid} AND position = -1;
