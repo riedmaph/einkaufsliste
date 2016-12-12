b89e50108f318c866a7fd0fd21741dcb9ab95020
gernot303 <mail@gernotbrunner.at>
2016-12-12 05:51:44 -0800

    *get fromposition by WITH statement

--- backend/server/controllers/items/moveItem.sql
+++ backend/server/controllers/items/moveItem.sql
@@ -1,29 +1,24 @@
 /* Beginn Transaction to secure atomic execution*/
 BEGIN;
-DO $$
 
-/* Select fromPosition by ID  */
-DECLARE fromposition INTEGER;
-BEGIN
-  fromposition :=
-  (SELECT Item.position
-		FROM ${schemaname:raw}.Item
-		WHERE id = ${id});
+/* select fromposition by ID*/
+WITH fromposition AS (
+  SELECT Item.position
+  FROM ${schemaname:raw}.Item
+  WHERE id = ${id})
 
 /* move items down to free targetposition position*/
 UPDATE ${schemaname:raw}.Item 
-SET position = position + (CASE fromposition < ${targetposition} WHEN true THEN -1 ELSE 1 END) 
+SET position = position + (CASE (SELECT * FROM fromposition) < ${targetposition} WHEN true THEN -1 ELSE 1 END) 
 WHERE list=${listid} AND 
-(CASE fromposition < ${targetposition} 
-	WHEN true THEN (position > fromposition AND position <= ${targetposition})
-	ELSE (position >= ${targetposition} AND position < fromposition)
+(CASE (SELECT * FROM fromposition) < ${targetposition} 
+  WHEN true THEN (position > (SELECT * FROM fromposition) AND position <= ${targetposition})
+  ELSE (position >= ${targetposition} AND position < (SELECT * FROM fromposition))
 END);
 
 /*set from item's position to targetposition position*/
 UPDATE ${schemaname:raw}.Item 
 SET position=${targetposition}
 WHERE id=${id};
-
-END $$;
 /*End Transaction*/
 COMMIT;
