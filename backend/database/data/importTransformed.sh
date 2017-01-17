c1ca35444d531660d3ee876dc9c3abda4973d035
Philipp R <badphil@googlemail.com>
2017-01-17 19:30:46 +0100

    refactored/fixed importTransformed

--- backend/database/data/importTransformed.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-. ../config.sh
-
-getDb "$1" dbhost dbport dbname
-getUser "crawler" dbusercrawler dbpasscrawler
-
-PGPASSWORD=$dbpasscrawler psql -h $dbhost -p $dbport -d $dbname -U $dbusercrawler -f "importTransformed.sql"
