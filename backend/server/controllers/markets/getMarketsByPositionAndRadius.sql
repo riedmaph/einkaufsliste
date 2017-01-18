2fa36ba95e5c273c06801fef1cea01eab64b7714
gernot303 <mail@gernotbrunner.at>
2017-01-18 11:27:05 -0800

    add shop to markets returned by /markets/search and /markets/favourites

--- backend/server/controllers/markets/getMarketsByPositionAndRadius.sql
+++ backend/server/controllers/markets/getMarketsByPositionAndRadius.sql
@@ -1,3 +1,3 @@
-SELECT market.id, market.name, market.latitude, market.longditude, market.street, market.zip, market.city
+SELECT market.id, market.name, market.latitude, market.longditude, market.street, market.zip, market.city, (CASE WHEN LOWER(market.name)LIKE'%rewe%'THEN 'REWE' ELSE 'EDEKA' END) as shop
 FROM Transformed.Market market
 WHERE earth_box(ll_to_earth(${latitude}, ${longditude}), ${maxdistance}) @> ll_to_earth(market.latitude, market.longditude);
--- backend/server/controllers/markets/getMarketsByZip.sql
+++ backend/server/controllers/markets/getMarketsByZip.sql
@@ -1,3 +1,3 @@
-SELECT market.id, market.name, market.latitude, market.longditude, market.street, market.zip, market.city
+SELECT market.id, market.name, market.latitude, market.longditude, market.street, market.zip, market.city, (CASE WHEN LOWER(market.name)LIKE'%rewe%'THEN 'REWE' ELSE 'EDEKA' END) as shop
 FROM Transformed.Market market
 WHERE market.zip = ${zip}
