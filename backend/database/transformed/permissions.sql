a91508efc8e19c09b72f0c422c2b3ecc20671de4
gernot303 <mail@gernotbrunner.at>
2017-01-24 12:07:27 -0800

    restore transformed folder

---
+++ backend/database/transformed/permissions.sql
@@ -0,0 +1,3 @@
+GRANT USAGE ON SCHEMA Crawled TO :dbusertransformer;
+GRANT SELECT ON ALL TABLES IN SCHEMA Crawled TO :dbusertransformer;
+GRANT SELECT ON ALL SEQUENCES IN SCHEMA Crawled TO :dbusertransformer;
