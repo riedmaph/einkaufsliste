78b32cc05c5d7962c4a8795230926b5d7d86d7ef
Stefan Cimander <stefan.cimander@me.com>
2017-02-02 13:59:45 +0100

    Move navigation to current offers to sidebar

--- frontend/webapp/src/app/components/offers/offers.component.ts
+++ frontend/webapp/src/app/components/offers/offers.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+} from '@angular/core';
 
 import { Offer } from '../../models';
+import { NavigationService } from '../../services';
 
 @Component({
   selector: 'sl-offers',
   templateUrl: 'offers.template.html',
   styleUrls: [ 'offers.style.scss' ],
 })
-export class OffersComponent {
+export class OffersComponent implements OnInit {
 
   /** Current offers at the user's favourite markets */
-  public offers: Offer[] = [ ];
+  private offers: Offer[] = [ ];
+
+  /**
+   * Constructor of the offers component.
+   */
+  constructor(
+    private navigationService: NavigationService,
+  ) { }
+
+  /**
+   * @memberof OnInit
+   */
+  public ngOnInit (): void {
+    this.navigationService.title = 'Current Offers';
+  }
 }
