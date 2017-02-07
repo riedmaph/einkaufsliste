3a85e574245e0ad6742ea9183f229ccbf02ea484
Philipp R <badphil@googlemail.com>
2017-02-07 09:33:25 +0100

    get markets by position: earthbox+distance

--- backend/server/controllers/markets/getMarketsByPositionAndRadius.sql
+++ backend/server/controllers/markets/getMarketsByPositionAndRadius.sql
@@ -9,5 +9,7 @@ SELECT
   (CASE WHEN LOWER(market.name)LIKE'%rewe%'THEN 'REWE' ELSE 'EDEKA' END) as shop,
   earth_distance(ll_to_earth(${latitude}, ${longitude}), ll_to_earth(market.latitude, market.longitude)) as distance
 FROM Grocerydata.Market market
-WHERE earth_distance(ll_to_earth(${latitude}, ${longitude}), ll_to_earth(market.latitude, market.longitude)) <  ${maxdistance}
+WHERE 
+	earth_box(ll_to_earth(${latitude}, ${longitude}), ${maxdistance}) @> ll_to_earth(market.latitude, market.longitude) -- Index search
+	and earth_distance(ll_to_earth(${latitude}, ${longitude}), ll_to_earth(market.latitude, market.longitude)) distance < ${maxdistance} -- Filter out superfluous results after index search
 ORDER BY  distance;
