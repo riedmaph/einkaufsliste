185c828cfce9c97c247ded1695d3e16427a05693
gernot303 <mail@gernotbrunner.at>
2017-02-07 10:40:31 -0800

    add title to offer

--- backend/server/controllers/markets/readOffers.sql
+++ backend/server/controllers/markets/readOffers.sql
@@ -1,4 +1,4 @@
-SELECT offer.id, offer.market, offer.offerprice, offer.offerfrom, offer.offerto, offer.discount, article.name as articlename, article.brand as articlebrand
+SELECT offer.id, offer.title, offer.market, offer.offerprice, offer.offerfrom, offer.offerto, offer.discount, article.name as articlename, article.brand as articlebrand
 FROM Grocerydata.Offer offer
 JOIN Grocerydata.Article article ON offer.article = article.id
 WHERE market = ${marketid}
