af2e23902f0dc50892aaf49621203fe5ee5ac155
Stefan Cimander <stefan.cimander@me.com>
2017-02-09 12:26:50 +0100

    Story #25: Display a list of current offers (#277)
    * Add offers page with according navigation item
    
    * Merge branch master into branch story-25
    
    * navigate to current offers from sidebar (#263)
    
    * load current offers (#265)
    
    * Load offers for favourite markets from offer service
    
    * Group offers by market and display offer details (#271)
    
    * add title to offer
    
    * add title to offer in market.js
    
    * Add offers to an available list (#273)
    
    * Add offer to the first of the user’s lists
    
    * Display menu asking which list to add the offer to
    
    * Extend offer title by article brand
    
    * Adjust offers to display new title
    
    * Inform user about how to add favourite market
    
    * Show hint that the length of a list name is limited

---
+++ frontend/webapp/src/app/components/offers/offers.component.ts
@@ -0,0 +1,89 @@
+import {
+  Component,
+  OnInit,
+} from '@angular/core';
+
+import {
+  OfferService,
+  ApiService,
+  ListCommunicationService,
+  NavigationService,
+} from '../../services';
+import {
+  Market,
+  Offer,
+  List,
+  ListItem,
+} from '../../models';
+
+@Component({
+  selector: 'sl-offers',
+  templateUrl: 'offers.template.html',
+  styleUrls: [ 'offers.style.scss' ],
+})
+export class OffersComponent implements OnInit {
+
+  /** Favourite markets of the user */
+  private markets: Market[] = [ ];
+
+  /** Current offers at the user's favourite markets */
+  private offers: Offer[] = [ ];
+
+  /**
+   * Constructor of the offers component.
+   */
+  constructor (
+    private apiService: ApiService,
+    private offerService: OfferService,
+    private listCommunicationService: ListCommunicationService,
+    private navigationService: NavigationService,
+  ) { }
+
+  /**
+   * Sets the navigation title and loads favorite markets
+   * as well as the according offers on init.
+   *
+   * @memberof OnInit
+   */
+  public ngOnInit () {
+    this.navigationService.title = 'Current Offers';
+    this.apiService.getFavouriteMarkets().subscribe(markets => {
+      this.markets = markets;
+      this.loadOffers(markets.map(market => market.id));
+    });
+  }
+
+  /**
+   * Adds an offer to a given list.
+   *
+   * @param {Offer} offer The offer to add to the given list.
+   * @param {List} list The list to add the offer to.
+   * @return {void}
+   */
+  public addOfferToList (offer: Offer, list: List): void {
+    const offerItem: ListItem = {
+        name: offer.brand + ' ' + offer.name,
+        unit: null,
+        amount: null,
+        onSale: true,
+        checked: false,
+      };
+    this.apiService.addItem(list.id, offerItem).subscribe();
+  }
+
+  /**
+   * Loads the offers for a given list of market identifiers and
+   * appends them to the current list of offers.
+   *
+   * @param {number[]} marketIds The list of market identifiers.
+   * @return {void}
+   */
+  private loadOffers (marketIds: number[]): void {
+    marketIds.map(marketUuid =>
+      this.offerService.getOffers(marketUuid).subscribe(offers =>
+        this.offers = this.offers.concat(offers)
+      )
+    );
+  }
+
+}
