cb8b4a5b5ea1dfad881ac7ca259118ef434584b3
gernot303 <mail@gernotbrunner.at>
2017-02-24 10:21:29 -0800

    add testcase

--- backend/server/test/dbhandler/dbhandler.js
+++ backend/server/test/dbhandler/dbhandler.js
@@ -4,6 +4,8 @@ var db = require(path.join('..', '..', 'controllers','dbconnector.js'));
 var sqlDeletetUser = db.loadSql(path.join('test', 'dbhandler', 'sqlDeletetUser.sql'));
 var sqlGetIdsOrderedByPositionList = db.loadSql(path.join('test', 'dbhandler', 'sqlGetIdsOrderedByPositionList.sql'));
 var sqlSetUpMoveItems = db.loadSql(path.join('test', 'dbhandler', 'setUpMoveItems.sql'));
+var sqlSetOptimisation = db.loadSql(path.join('test', 'dbhandler', 'setOptimisation.sql'));
+
 
 module.exports.deleteUsers = function (callback) {
   db.conn.none(sqlDeletetUser)
@@ -41,3 +43,13 @@ module.exports.getIdsOrderedByPositionList = function (listid, callback) {
    });
 }
 
+module.exports.setOptimisation = function (callback) {
+  db.conn.none(sqlSetOptimisation)
+    .then(function () {
+      callback();
+    })
+    .catch(function (err) {
+      console.log(err.message);
+      callback(err);
+    });
+}
