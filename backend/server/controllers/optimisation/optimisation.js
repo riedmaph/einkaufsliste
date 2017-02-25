a06d3216b590615f31307f0aac6d4ac6ef001dea
gernot303 <mail@gernotbrunner.at>
2017-02-25 00:03:01 -0800

    add methods to waterfall

--- backend/server/controllers/optimisation/optimisation.js
+++ backend/server/controllers/optimisation/optimisation.js
@@ -15,56 +15,60 @@ function updateItem(req, res, next) {
 
 }
 
+function saveOptimisedList(req, res, next) {
+
+}
+
 function getOptimisedList(req, res, next) {
   var options = {};
   options.listid = req.params.listid;
   options.userid = req.body.userid
 
   waterfall([
-    async.apply(initialize, options)
-    //executeOptimisation(oList),
-    //persistOptimisedList(oList)
-  ], function (err, oList) {
+    async.apply(initialize, options), //async.apply to hand over parameter to first method
+    executeOptimisation,
+    createOptimisedList
+  ], function (err, itemList) {
       if(!err) {
         res.status(200)
         .json(
-          oList
+          itemList
         );
       }
       else {
-        console.log('errorhandling');
         next(err);    
       }
   });
 }
 
+//load items for given list and suitable offers for each item
 function initialize(options, callback) {
-   db.conn.task(function (t) {
-    return t.map(sqlReadItems, options, function(item) { //load all items for list
-      options.name = item.name;
-      return t.any(sqlFindOffers, options)  //load offers for each item
-        .then(function(offers) {
-          offers = offers.map(
-            function(offer) 
-            {
-              return {
-                        id: offer.id, 
-                        market: offer.market, 
-                        offerprice: offer.offerprice, 
-                        offerfrom: offer.offerfrom, 
-                        offerto: offer.offerto, 
-                        discount: offer.discount, 
-                        isOptimium: false,
-                        article:{
-                          name: offer.articlename, 
-                          brand: offer.articlebrand
+    db.conn.task(function (t) {
+      return t.map(sqlReadItems, options, function(item) { //load all items for list
+        options.name = item.name;
+        return t.any(sqlFindOffers, options)  //load offers for each item
+          .then(function(offers) {
+            offers = offers.map(
+              function(offer) 
+              {
+                return {
+                          id: offer.id, 
+                          market: offer.market, 
+                          offerprice: offer.offerprice, 
+                          offerfrom: offer.offerfrom, 
+                          offerto: offer.offerto, 
+                          discount: offer.discount, 
+                          isOptimium: false,
+                          article:{
+                            name: offer.articlename, 
+                            brand: offer.articlebrand
+                          }
                         }
-                      }
-            }
-          );
-          item.offers = offers;
-          return item;
-        });
+              }
+            );
+            item.offers = offers;
+            return item;
+          });
     }).then(t.batch)
    })
    .then(function (data) {
@@ -72,7 +76,7 @@ function initialize(options, callback) {
       itemList = {
           items: data
         };
-    callback(null, itemList);
+    callback(null, itemList, options);
   })
   .catch(function (err) {
     err.message = 'controllers.optimisation.loadList: ' + err.message;
@@ -80,16 +84,19 @@ function initialize(options, callback) {
   });
 }
 
-function executeOptimisation(oList, method, callback) {
+//select the right optimisation-method
+function executeOptimisation(itemList, options, callback) {
   //call right optimisation
+  callback(null, itemList, options);
 }
 
-function optimiseByPrice(oList) {
+function _optimiseByPrice(itemList) {
 
 }
 
-function persistOptimisedList(oList, callback) {
-
+function createOptimisedList(itemList, options, callback) {
+  //write optimsed list to db 
+  callback(null, itemList);
 }
 
 module.exports = {
