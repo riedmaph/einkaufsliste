35db634e7dab0ea8a2b4210eac769f424290ef62
riedmaph <riedmaph@users.noreply.github.com>
2017-02-28 14:57:31 +0100

    return shared attribute at getList API call (#296)

--- backend/server/controllers/lists/readList.sql
+++ backend/server/controllers/lists/readList.sql
@@ -1,3 +1,9 @@
-SELECT list.id as id, list.name as name, CAST((SELECT COUNT(*) FROM ${schemaname:raw}.Item item WHERE item.list = list.id) as int) as count 
-FROM ${schemaname:raw}.List list
-WHERE list.id = ${id}
+SELECT 
+	list.id as id, 
+	list.name as name, 
+	CAST((SELECT COUNT(*) FROM ${schemaname:raw}.Item item WHERE item.list = list.id) as int) as count,
+	exists (select enduser from ${schemaname:raw}.listshare s where s.list=list.id ) as shared
+FROM 
+	${schemaname:raw}.List list
+WHERE 
+	list.id = ${id}
