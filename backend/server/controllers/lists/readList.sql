ca36db24f986292460ca7e6d48b8185768b08309
Michael Schreier <schreier@in.tum.de>
2017-03-04 00:44:52 +0100

    add safety check for correct user in getList request

--- backend/server/controllers/lists/readList.sql
+++ backend/server/controllers/lists/readList.sql
@@ -1,3 +1,4 @@
 SELECT list.id as id, list.name as name, CAST((SELECT COUNT(*) FROM ${schemaname:raw}.Item item WHERE item.list = list.id) as int) as count 
 FROM ${schemaname:raw}.List list
 WHERE list.id = COALESCE(${id}, (SELECT enduser.recentlist FROM ${schemaname:raw}.EndUser WHERE enduser.id = ${userid}))
+AND list.enduser = ${userid}
