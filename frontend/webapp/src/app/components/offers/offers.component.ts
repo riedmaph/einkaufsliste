fbfef634d37082229818354f456b54f325488611
Thomas Weber <weber@openinnovator.net>
2017-03-04 13:59:36 +0100

    Fix previous fix

--- frontend/webapp/src/app/components/offers/offers.component.ts
+++ frontend/webapp/src/app/components/offers/offers.component.ts
@@ -32,9 +32,6 @@ export class OffersComponent implements OnInit {
   /** Current offers at the user's favourite markets */
   private offers: Offer[] = [ ];
 
-  /** Subset of all offers */
-  private selectedOffers: Offer[] = [ ];
-
   /**
    * Constructor of the offers component.
    */
@@ -44,6 +41,14 @@ export class OffersComponent implements OnInit {
     private offerService: OfferService,
     private listCommunicationService: ListCommunicationService,
   ) { }
+
+  /** Subset of all offers */
+  public get selectedOffers (): Offer[] {
+    return this.filterQuery.length ?
+      this.offers
+        .filter(offer => offer.title.toLowerCase().includes(this.filterQuery.toLowerCase())) :
+      this.offers;
+  }
 
   /**
    * Sets the navigation title and loads favorite markets
@@ -77,22 +82,6 @@ export class OffersComponent implements OnInit {
   }
 
   /**
-   * Sets selectedOffers to all offers matching the query text
-   *
-   * @return {void}
-   */
-  public filter (): void {
-    if (this.filterQuery.length) {
-      const query = this.filterQuery.toLowerCase();
-      this.selectedOffers = this.offers.filter(offer =>
-        offer.title.toLowerCase().includes(query)
-      );
-    } else {
-      this.selectedOffers = this.offers;
-    }
-  }
-
-  /**
    * Loads the offers for a given list of market identifiers and
    * appends them to the current list of offers.
    *
@@ -103,7 +92,6 @@ export class OffersComponent implements OnInit {
     marketIds.map(marketUuid =>
       this.offerService.getOffers(marketUuid).subscribe(offers => {
         this.offers = this.offers.concat(offers);
-        this.filter();
       }),
     );
   }
