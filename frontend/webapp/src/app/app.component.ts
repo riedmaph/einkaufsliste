1e18c459278fad6c5a19ca5b0e84c6df48aaec3e
Thomas Weber <elordin@users.noreply.github.com>
2017-03-04 11:27:38 +0100

    Fix nav and sidenav (#308)
    * Update list item editing
    
    * add support to check item name max length inside the server (#301)
    
    * Fix missing edit requirements (#300)
    
    * Commit on enter
    * maxLength restriction
    * close edit mode on checking an item
    
    Closes #5
    
    * Rearrange nav code layout to accomodate sidenav backdrop
    
    * Fix navigation service
    
    * Add sticky header and fix scroll behaviour
    
    * Fix shared-indicator CSS

--- frontend/webapp/src/app/app.component.ts
+++ frontend/webapp/src/app/app.component.ts
@@ -1,7 +1,12 @@
 import {
   Component,
+  ViewChild,
   ViewEncapsulation,
 } from '@angular/core';
+import { MdSidenav } from '@angular/material';
+import { Router } from '@angular/router';
+
+import { AuthService } from './services';
 
 /*
  * App Component
@@ -16,4 +21,32 @@ import {
   ],
   templateUrl: 'app.template.html',
 })
-export class AppComponent { }
+export class AppComponent {
+
+  @ViewChild(MdSidenav)
+  public sidenav: MdSidenav;
+
+  constructor (
+    public authService: AuthService,
+    public router: Router,
+  ) {
+    this.router.events.subscribe(_ => {
+      this.closeSidenav();
+    });
+  }
+
+  /** Closes the sidenav if present */
+  public closeSidenav () {
+    if (this.sidenav) {
+      this.sidenav.close();
+    }
+  }
+
+  /** Opens the sidenav if present */
+  public openSidenav () {
+    if (this.sidenav) {
+      this.sidenav.open();
+    }
+  }
+
+}
